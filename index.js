727060334be91439106e9b2aea0dab5100016645
Xiao Jin <miushock@gmail.com>
2018-08-16 17:53:13 +0800

    wrap up frist useful publish

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const fs = require('fs');
 
-var csv = './event_def.csv'
+var csv = __dirname + '/event_def.csv'
 
-var readDef = function (){
+var importDef = function (){
   return new Promise (function(resolve, reject){
     fs.readFile(csv, 'utf8', (err, data) => {
         err ? reject(err) : resolve(data);
@@ -11,38 +11,37 @@ var readDef = function (){
   });
 }
 
-var parseDef = function(){
-  return readDef().then(
-    (data) => {
-      let lines = data.split(/\r?\n/);
-      if (lines.length <= 1) {
-        throw 'improper source';
-      }
-      let attrs = lines[0].split('\,');
+var parseDef = (data) => {
+  let lines = data.split(/\r?\n/);
+  if (lines.length <= 1) {
+    throw 'improper source';
+  }
+  let attrs = lines[0].split('\,');
 
-      let events = [];
-      lines.slice(1, lines.length-1).forEach((curr_line) => {
-        let entries = curr_line.split(',');
-        let entry = {};
-        for (let i = 0; i < attrs.length; i++) {
-          entry[attrs[i]] = entries[i];
-        }
-        events.push(entry);
-      });
-      return events;
-    },
-    (err) => {
-      throw err;
+  let events = [];
+  lines.slice(1, lines.length-1).forEach((curr_line) => {
+    let entries = curr_line.split(',');
+    let entry = {};
+    for (let i = 0; i < attrs.length; i++) {
+      entry[attrs[i]] = entries[i];
     }
-  );
+    events.push(entry);
+  });
+  return events;
 }
 
-parseDef()
-  .then(
-    (events)=>{
-      console.log(events);
-    },
-    (err)=>{
-      console.log(err);
-    }
-  );
+function EventDefinitions(){}
+
+
+EventDefinitions.JSONDef = function(){
+  return importDef()
+  .then(parseDef)
+  .then((data) => {
+    return data.filter((item) => {
+      return item['Code'].length > 0;
+    });
+  })
+  .catch(err => console.log(err));
+}
+
+exports.EventDefinitions = EventDefinitions;
